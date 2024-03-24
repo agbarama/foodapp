40756d071e5486386038da41c030ad0dbcd22911
agbarama <agbaramafavour4@gmail.com>
2024-03-24 09:27:21 +0100

    Created  FoodList component and mapped through foodData array.

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,19 @@ import Navbar from "./components/Navbar";
 import Search from "./components/Search";
 import Container from "./components/Container";
 import InnerContainer from "./components/InnerContainer";
+import FoodList from "./components/FoodList";
 
 function App() {
   const [foodData, setFoodData] = useState([]);
+  const [foodId, setFoodId] = useState(656329);
   return (
     <>
       <Navbar />
       <Search foodData={foodData} setFoodData={setFoodData} />
       <Container>
-        <InnerContainer></InnerContainer>
+        <InnerContainer>
+          <FoodList foodData={foodData} setFoodId={setFoodId} />
+        </InnerContainer>
       </Container>
     </>
   );
