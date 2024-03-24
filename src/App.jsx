b6b889c07cee6d0e19e80c05223df97ff32fef10
agbarama <agbaramafavour4@gmail.com>
2024-03-24 08:47:09 +0100

    Created and styled the Search component.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
+import "./App.css";
 import Navbar from "./components/Navbar";
+import Search from "./components/Search";
 
 function App() {
+  const [foodData, setFoodData] = useState([]);
   return (
     <>
       <Navbar />
+      <Search foodData={foodData} setFoodData={setFoodData} />
     </>
   );
 }
