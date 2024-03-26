aa9cc04b634b410108fa35f9a1ef06215e75d100
agbarama <agbaramafavour4@gmail.com>
2024-03-26 11:27:49 +0100

    Auth trial

--- src/components/FoodDetails.jsx
+++ src/components/FoodDetails.jsx
@@ -12,8 +12,10 @@ const FoodDetails = ({ foodId }) => {
   const [isLoading, setIsLoading] = useState(true);
 
   const URL = `https://api.spoonacular.com/recipes/${foodId}/information`;
+  const API_Key = process.env.VITE_SOME_KEY;
+
   // const API_Key = process.env.API_KEY
-  const API_Key = import.meta.env.VITE_SOME_KEY;
+  // const API_Key = import.meta.env.VITE_SOME_KEY;
 
   useEffect(() => {
     async function fetchRecipe() {
